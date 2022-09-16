a78a9d0f4b4fea448bf2b8a96a9c988d48545ab5
ehtesham25 <ehteshamulhaqkhan25@gmail.com>
2022-09-16 23:05:06 +0500

    our services component

---
+++ src/components/OurServices.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { FaExchangeAlt, FaDiceD20, FaRegHandPointRight } from "react-icons/fa"
+
+import Card from './Card'
+
+const OurServices = () => {
+    return (
+        <div className='w-full bg-white py-10'>
+            <div className='flex flex-col justify-center items-center py-4'>
+                <p className='text-2xl text-btnloginColor font-semibold'>Our Services</p>
+                <p className='w-[450px] text-center py-2'>All our accounting services are built around YOUR NEEDS in mind.Select option below to see our services for YOU</p>
+                <div className='flex w-full px-16 items-center'>
+                    <Card Icon={FaDiceD20} title="Accounting Services" />
+                    <Card Icon={FaExchangeAlt} title="Tax Services" />
+                    <Card Icon={FaRegHandPointRight} title="Company Secretarial" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default OurServices
