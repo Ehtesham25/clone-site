951cd263c1f0dbd3fd7cb1a6d69a9c771363d91b
Ehtesham Khan <72070084+Ehtesham25@users.noreply.github.com>
2023-09-13 20:24:57 -0700

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -4,4 +4,4 @@ export { default as About } from "./About";
 export { default as OurServices } from "./OurServices";
 export { default as Accounting } from "./Accounting";
 export { default as Work } from "./Work";
-export { default as Footer } from "./Footer"
+export { default as Footer } from "./Footer";
